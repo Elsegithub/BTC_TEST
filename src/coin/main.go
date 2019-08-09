641f24b557f80046ebc0f8acec7844a75b3aaed0
lys <787640830@qq.com>
2019-08-09 11:33:03 +0800

    工作量计算

--- src/coin/main.go
+++ src/coin/main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"core"
 	"fmt"
+	"strconv"
 )
 
-func main()  {
+func main() {
 	bc := core.NewBlockChain()
 	bc.AddBlock("send 1 BTC to ME")
 	bc.AddBlock("send 2 more BTC to ME")
-	for _, block := range bc.Blocks{
+	for _, block := range bc.Blocks {
 		fmt.Printf("Prev.hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := core.NewProofOfWork(block)
+		fmt.Printf("Pow: %s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Println()
 	}
 }
--- src/core/block.go
+++ src/core/block.go
@@ -8,25 +8,29 @@ import (
 )
 
 type Block struct {
-	Timestamp	int64 //时间戳
-	Data		[]byte  //数据
-	PrevBlockHash []byte  //前一块hash
-	Hash		[]byte  //hash
+	Timestamp     int64  //时间戳
+	Data          []byte //数据
+	PrevBlockHash []byte //前一块hash
+	Hash          []byte //hash
+	Nonce         int
 }
 
-func NewBlock(data string, prevBlockHash []byte) *Block{
-	block := Block{Timestamp:time.Now().Unix(), Data:[]byte(data), PrevBlockHash:prevBlockHash, Hash:[]byte{}}
-	block.SetHash()
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := Block{Timestamp: time.Now().Unix(), Data: []byte(data), PrevBlockHash: prevBlockHash, Hash: []byte{}}
+	pow := NewProofOfWork(&block)
+	nonce, hash := pow.Run()
+	block.Hash = hash[:]
+	block.Nonce = nonce
 	return &block
 }
 
-func (b *Block) SetHash(){
+func (b *Block) SetHash() {
 	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
 	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
 	hash := sha256.Sum256(headers)
 	b.Hash = hash[:]
 }
 
-func NewFirstBlock() *Block{
-	return  NewBlock("first block", []byte{})
+func NewFirstBlock() *Block {
+	return NewBlock("first block", []byte{})
 }
